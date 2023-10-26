92beecab30128e8f6d437109906a4f7f3fdbd971
Prashant-S29 <prashant.s2922@gmail.com>
2023-10-26 22:17:01 +0530

    new changes introduced

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <div>Hello World. Changes made by Prashant. This is the second time i am making the changes.</div>
+      <div>
+        Hello World. Changes made by Prashant. This is the second time i am
+        making the changes.
+      </div>
+      <div><span>GHEdkjwhf e </span></div>
     </>
-  )
+  );
 }
